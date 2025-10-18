bcb048764fa8a0d5daafe7bb0e98c9ea2966347d
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-18 15:54:55 +0530

    custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SingupComponent } from "./singup/singup.component";
+import { ProfileComponent } from "./profile/profile.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, SingupComponent],
+  imports: [RouterOutlet, LoginComponent, SingupComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
