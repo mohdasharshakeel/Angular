078f845a3de49db3c988e70344ca1ae1c698f851
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-18 15:48:37 +0530

    sign up component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SingupComponent } from "./singup/singup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, SingupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
