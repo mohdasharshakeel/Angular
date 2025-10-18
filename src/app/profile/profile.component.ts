ff08e0bce098c99515597122722014e9bf590f2c
mohdasharshakeel <mohdasharshakeel@gmail.com>
2025-10-18 18:18:41 +0530

    custom files

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-profile',
-    template: '<h1>Profile Component</h1>'
+    // template: '<h1>Profile Component</h1>'
+    templateUrl: './profile.component.html',
+    
 })
 export class ProfileComponent {
 
